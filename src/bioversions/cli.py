25f7237d79bf1fa08c2495377278899a01050ae4
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-07 18:49:42 +0100

    Switch to click-default-group

--- src/bioversions/cli.py
+++ src/bioversions/cli.py
@@ -3,10 +3,16 @@
 """Command line interface for bioversions."""
 
 import click
+from click_default_group import DefaultGroup
 
 
-@click.command()
+@click.group(cls=DefaultGroup, default='web', default_if_no_args=True)
 def main():
+    """The bioversions CLI."""
+
+
+@main.command()
+def web():
     """Run the bioversions web application."""
     from .wsgi import app
     app.run()
