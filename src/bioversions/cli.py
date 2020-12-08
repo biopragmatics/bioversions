8faf62328b8562bc70870019d5cf0ed1aa73d1d0
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-08 12:54:10 +0100

    Better click stuff

--- src/bioversions/cli.py
+++ src/bioversions/cli.py
@@ -4,6 +4,8 @@
 
 import click
 from click_default_group import DefaultGroup
+from more_click import make_web_command, verbose_option
+from tabulate import tabulate
 
 
 @click.group(cls=DefaultGroup, default='web', default_if_no_args=True)
@@ -11,15 +13,18 @@ def main():  # noqa:D401
     """The bioversions CLI."""
 
 
-@main.command()
-def web():
-    """Run the bioversions web application."""
-    from .wsgi import app
-    app.run()
+web = make_web_command(
+    app='bioversions.wsgi:app',
+    group=main,
+    command_kwargs=dict(
+        help='Run the bioversions web application.',
+    ),
+)
 
 
 @main.command()
 @click.argument('key')
+@verbose_option
 def get(key: str):
     """Print the version."""
     from . import get_version
