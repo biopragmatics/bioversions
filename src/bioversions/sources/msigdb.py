36f523b5149bae53d920e5965be068ebc9ffa74f
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-22 02:06:33 +0100

    Update msigdb.py

--- src/bioversions/sources/msigdb.py
+++ src/bioversions/sources/msigdb.py
@@ -22,12 +22,13 @@ class MSigDBGetter(Getter):
         """Get the latest MSigDB version number."""
         soup = get_soup(URL)
 
-        x = soup.find(text='Current Version')
-        paragraph = x.parent.find_next_sibling('p')
-        date = paragraph.text.strip()[len('MSigDB database v7.2 updated '):].split('.')[0]
-
         header = soup.find('h1')
         version = header.text.split(' ')[-1].lstrip('v')
+
+        x = soup.find(text='Current Version')
+        paragraph = x.parent.find_next_sibling('p')
+        date = paragraph.text.strip()[len(f'MSigDB database v{version} updated '):].split('.')[0]
+
         return dict(date=date, version=version)
 
 
