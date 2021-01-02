e25d00352ef75b129805620185d1e2c8beca2a09
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-02 16:55:32 +0100

    Get date of InterPro release

--- src/bioversions/sources/interpro.py
+++ src/bioversions/sources/interpro.py
@@ -2,11 +2,17 @@
 
 """A getter for InterPro."""
 
+import re
+
+import requests
+
 from bioversions.utils import Getter, _get_ftp_version
 
 __all__ = [
     'InterProGetter',
 ]
+
+SOLVE_RE = re.compile(r'(\d)(st|nd|rd|th)')
 
 
 class InterProGetter(Getter):
@@ -14,10 +20,23 @@ class InterProGetter(Getter):
 
     name = 'InterPro'
     homepage_fmt = 'ftp://ftp.ebi.ac.uk/pub/databases/interpro/{version}/'
+    date_fmt = '%d %B %Y'
 
     def get(self):
         """Get the latest InterPro version number."""
-        return _get_ftp_version('ftp.ebi.ac.uk', 'pub/databases/interpro/')
+        version = _get_ftp_version('ftp.ebi.ac.uk', 'pub/databases/interpro/')
+
+        with requests.Session() as session:
+            res = session.get('ftp://ftp.ebi.ac.uk/pub/databases/interpro/current/release_notes.txt')
+            for line in res.iter_lines():
+                line = line.decode('utf8').strip()
+                if line.startswith('Release') and not line.startswith('Release Notes'):
+                    return dict(version=version, date=_process_line(line))
+        raise ValueError
+
+
+def _process_line(s: str) -> str:
+    return SOLVE_RE.sub(r'\1', s.split(',')[1].strip())
 
 
 if __name__ == '__main__':
