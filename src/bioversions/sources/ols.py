292c0295118786bf95509c4e8100de3444b49b01
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-06-14 13:50:21 -0400

    Update bioregistry

--- src/bioversions/sources/ols.py
+++ src/bioversions/sources/ols.py
@@ -6,24 +6,24 @@ import logging
 from typing import Iterable, Mapping, Optional, Type, Union
 
 import bioregistry
-from bioregistry.external.ols import get_ols
-from bioregistry.resolve import _clean_version, get_name
+from bioregistry.data import get_ols_processing
+from bioregistry.resolve import get_name
 
 from bioversions.utils import Getter, VersionType
 
 logger = logging.getLogger(__name__)
 
-bioregistry_id_to_ols_id = {
-    bioregistry_id: bioregistry_entry['ols']['prefix']
-    for bioregistry_id, bioregistry_entry in bioregistry.read_bioregistry().items()
-    if 'ols' in bioregistry_entry
-}
+ols_processing = get_ols_processing()
 
 
-def _get_version_type(bioregistry_id) -> Optional[VersionType]:
-    ols_entry = bioregistry.get(bioregistry_id)
-    ols_version_type = ols_entry.get('ols_version_type')
-    ols_version_date_format = ols_entry.get('ols_version_date_format')
+def _get_version_type(bioregistry_id: str) -> Optional[VersionType]:
+    ols_id = bioregistry.get_ols_prefix(bioregistry_id)
+    ols_config = ols_processing.get(ols_id)
+    if ols_config is None:
+        raise
+
+    ols_version_type = ols_config.version_type
+    ols_version_date_format = ols_config.version_date_format
     if ols_version_date_format:
         return VersionType.date
     elif ols_version_type:
@@ -32,19 +32,17 @@ def _get_version_type(bioregistry_id) -> Optional[VersionType]:
         logger.warning('[%s] missing version type', bioregistry_id)
 
 
-def make_ols_getter(ols, bioregistry_id: str) -> Optional[Type[Getter]]:
+def make_ols_getter(bioregistry_id: str) -> Optional[Type[Getter]]:
     """Make a getter from OLS."""
-    ols_id = bioregistry_id_to_ols_id.get(bioregistry_id)
+    ols_id = bioregistry.get_ols_prefix(bioregistry_id)
     if ols_id is None:
-        logger.warning('[%s] no OLS id', bioregistry_id)
         return
 
-    version = ols[ols_id]['config'].get('version')
+    version = bioregistry.get(bioregistry_id).ols.get('version')
     if version is None:
         logger.debug('[%s] no OLS version', bioregistry_id)
         return
 
-    version = _clean_version(bioregistry_id, version)
     _brid = bioregistry_id
 
     class OlsGetter(Getter):
@@ -63,10 +61,8 @@ def make_ols_getter(ols, bioregistry_id: str) -> Optional[Type[Getter]]:
 
 def iter_ols_getters(*, force_download: bool = True) -> Iterable[Type[Getter]]:
     """Iterate over OLS getters."""
-    ols = get_ols(force_download=force_download, mappify=True)
-
-    for bioregistry_id in bioregistry_id_to_ols_id:
-        yv = make_ols_getter(ols=ols, bioregistry_id=bioregistry_id)
+    for bioregistry_id in bioregistry.read_registry():
+        yv = make_ols_getter(bioregistry_id)
         if yv is not None:
             yield yv
 
