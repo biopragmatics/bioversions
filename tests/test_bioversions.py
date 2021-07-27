456634e3c6672fc917df1c52be8fc6e1eb66c013
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-27 13:12:23 +0200

    Add more tests

--- tests/test_bioversions.py
+++ tests/test_bioversions.py
@@ -5,11 +5,23 @@
 import datetime
 import unittest
 
+import bioversions
 from bioversions.sources import BioGRIDGetter, WikiPathwaysGetter
 
 
 class TestGetter(unittest.TestCase):
     """Tests for the Getter class."""
+
+    def test_get(self):
+        """Test getters."""
+        prefixes = [
+            "reactome",
+            "kegg",
+        ]
+        for prefix in prefixes:
+            with self.subTest(prefix=prefix):
+                s = bioversions.get_version(prefix)
+                self.assertIsInstance(s, str)
 
     def test_getter(self):
         """Test the BioGRID getter."""
