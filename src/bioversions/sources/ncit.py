8bc9702d6bddb0859abcdb312a93d7a6e3e971d0
James Stevenson <james.sharpsteen@gmail.com>
2021-09-21 23:37:51 -0400

    Fix formatting

--- src/bioversions/sources/ncit.py
+++ src/bioversions/sources/ncit.py
@@ -11,8 +11,10 @@ __all__ = [
 ]
 
 URL = "https://ncithesaurus.nci.nih.gov/ncitbrowser/"
-PATTERN = re.compile(r"Version:([0-9]{2}\.[0-9]{2}[a-z]) "
-                     r"\(Release date:([0-9]{4}-[0-9]{2}-[0-9]{2})")
+PATTERN = re.compile(
+    r"Version:([0-9]{2}\.[0-9]{2}[a-z]) " r"\(Release date:([0-9]{4}-[0-9]{2}-[0-9]{2})"
+)
+
 
 class NCItGetter(Getter):
     """A getter for the NCI Thesaurus."""
@@ -25,7 +27,7 @@ class NCItGetter(Getter):
     def get(self) -> Dict[str, str]:
         """Get the latest NCIt version number."""
         soup = get_soup(URL)
-        version_str = soup.find('span', {'class': 'vocabularynamelong_ncit'}).contents[0]
+        version_str = soup.find("span", {"class": "vocabularynamelong_ncit"}).contents[0]
         match = re.search(PATTERN, version_str)
         return {
             "version": match.group(1),
