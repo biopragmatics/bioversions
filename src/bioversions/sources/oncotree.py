b00c1b920dc807eed7e78bf660e9761b81c8d8c4
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-10-03 17:12:20 -0400

    Add oncotree homepage

--- src/bioversions/sources/oncotree.py
+++ src/bioversions/sources/oncotree.py
@@ -9,25 +9,29 @@ __all__ = [
     "OncoTreeGetter",
 ]
 
-URL = "http://oncotree.mskcc.org/api/versions"
-
 
 class OncoTreeGetter(Getter):
     """A getter for OncoTree."""
 
     bioregistry_id = "oncotree"
     name = "OncoTree"
+    homepage_fmt = "http://oncotree.mskcc.org/api/tumorTypes?version=oncotree_{version}"
     version_type = VersionType.date
     date_version_fmt = "%Y-%m-%d"
 
     def get(self) -> str:
         """Get the latest OncoTree version number."""
-        res = requests.get(URL, params={"format": "json"})
+        res = requests.get("http://oncotree.mskcc.org/api/versions", params={"format": "json"})
         res_json = res.json()
         version = next(
             (r["release_date"] for r in res_json if r["api_identifier"] == "oncotree_latest_stable")
         )
         return version
+
+    @staticmethod
+    def homepage_version_transform(version: str) -> str:
+        """Reformat version value for OncoTree data URL."""
+        return version.replace("-", "_")
 
 
 if __name__ == "__main__":
