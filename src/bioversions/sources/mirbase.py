c226156d66c461dd88a022ce4261371a45df6dd9
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-05 14:04:43 +0100

    Fix miRBase getter

--- src/bioversions/sources/mirbase.py
+++ src/bioversions/sources/mirbase.py
@@ -4,7 +4,7 @@
 
 import ftplib
 
-from bioversions.utils import Getter, VersionType
+from bioversions.utils import Getter, VersionType, get_soup
 
 __all__ = [
     "MirbaseGetter",
@@ -18,18 +18,17 @@ class MirbaseGetter(Getter):
 
     bioregistry_id = "mirbase"
     name = "miRBase"
-    homepage_fmt = "ftp://mirbase.org/pub/mirbase/{version}/"
+    homepage_fmt = "https://www.mirbase.org/ftp/{version}/"
     version_type = VersionType.semver_minor
 
     def get(self):
         """Get the latest miRBase version number."""
-        with ftplib.FTP("mirbase.org") as ftp:
-            ftp.login()
-            ftp.cwd("pub/mirbase/CURRENT")
-            for name, _ in ftp.mlsd():
-                if name.startswith(PREFIX):
-                    return name[len(PREFIX) :]
-        raise ValueError
+        url = "https://www.mirbase.org/ftp/CURRENT/"
+        soup = get_soup(url)
+        rows = list(soup.find_all("tr"))
+        cells = list(rows[3].find_all("td"))
+        text = cells[1].find("a").text
+        return text[len(PREFIX) :]
 
 
 if __name__ == "__main__":
