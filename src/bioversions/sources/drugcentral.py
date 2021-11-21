355616caa4674701db9450318b2f359dc75adb84
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-21 15:43:13 +0100

    Fix DrugCentral

--- src/bioversions/sources/drugcentral.py
+++ src/bioversions/sources/drugcentral.py
@@ -16,16 +16,15 @@ class DrugCentralGetter(Getter):
 
     bioregistry_id = "drugcentral"
     name = "DrugCentral"
-    date_version_fmt = "%m/%d/%Y"
+    date_version_fmt = "%Y_%m_%d"
+    homepage_fmt = href = "https://unmtid-shinyapps.net/download/DrugCentral/{version}"
     version_type = VersionType.date
 
     def get(self) -> str:
         """Get the latest DrugCentral version number."""
         soup = get_soup(URL)
-        manifest = (
-            soup.find("section").find(**{"class": "container"}).find("h4").find("a").find("u")
-        )
-        return manifest.text.split(" ")[-1]
+        manifest = soup.find("h4", **{"class": "txt-sec"}).find("a")
+        return manifest.attrs["href"].split("/")[-2]
 
 
 if __name__ == "__main__":
