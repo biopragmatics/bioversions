bb1dfae1cadef43fc7f2b3a515eaee355ff9ea75
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-17 22:43:44 +0100

    Update DrugCentral version getter

--- src/bioversions/sources/drugcentral.py
+++ src/bioversions/sources/drugcentral.py
@@ -2,13 +2,20 @@
 
 """A getter for DrugCentral."""
 
-from bioversions.utils import Getter, VersionType, get_soup
+from contextlib import closing
+
+from bioversions.utils import Getter, VersionType
 
 __all__ = [
     "DrugCentralGetter",
 ]
 
-URL = "https://drugcentral.org/download"
+HOST = "unmtid-dbs.net"
+PORT = 5433
+USER = "drugman"
+PASSWORD = "dosage"
+DBNAME = "drugcentral"
+PARAMS = dict(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
 
 
 class DrugCentralGetter(Getter):
@@ -16,15 +23,23 @@ class DrugCentralGetter(Getter):
 
     bioregistry_id = "drugcentral"
     name = "DrugCentral"
-    date_version_fmt = "%Y_%m_%d"
-    homepage_fmt = href = "https://unmtid-shinyapps.net/download/DrugCentral/{version}"
+    date_fmt = "%Y-%m-%d"
     version_type = VersionType.date
 
-    def get(self) -> str:
+    def get(self):
         """Get the latest DrugCentral version number."""
-        soup = get_soup(URL)
-        manifest = soup.find("h4", **{"class": "txt-sec"}).find("a")
-        return manifest.attrs["href"].split("/")[-2]
+        import psycopg2
+
+        with closing(psycopg2.connect(**PARAMS)) as conn:
+            with closing(conn.cursor()) as cur:
+                cur.execute("SELECT version, dtime FROM public.dbversion")
+                version, dtime = cur.fetchone()
+
+        # TODO update return format to allow datetime type
+        return {
+            "version": str(version),
+            "date": dtime.strftime(self.date_fmt),
+        }
 
 
 if __name__ == "__main__":
