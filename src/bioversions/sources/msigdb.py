7653cd904c96cd1ac31a31daa4235b351254979b
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-02-25 11:03:52 +0100

    Update msigdb.py

--- src/bioversions/sources/msigdb.py
+++ src/bioversions/sources/msigdb.py
@@ -2,8 +2,6 @@
 
 """A getter for MSigDB."""
 
-from typing import Mapping
-
 from bioversions.utils import Getter, VersionType, get_soup
 
 __all__ = [
@@ -19,21 +17,16 @@ class MSigDBGetter(Getter):
     bioregistry_id = "msigdb"
     name = "MSigDB"
     homepage_fmt = "https://data.broadinstitute.org/gsea-msigdb/msigdb/release/{version}"
-    date_fmt = "%B %Y"
-    version_type = VersionType.month
+    version_type = VersionType.year_minor
 
-    def get(self) -> Mapping[str, str]:
+    def get(self):
         """Get the latest MSigDB version number."""
         soup = get_soup(URL)
 
-        header = soup.find("h1")
-        version = header.text.split(" ")[-1].lstrip("v")
-
         x = soup.find(text="Current Version")
         paragraph = x.parent.find_next_sibling("p")
-        date = paragraph.text.strip()[len(f"MSigDB database v{version} updated ") :].split(".")[0]
-
-        return dict(date=date, version=version)
+        version = paragraph.text.strip().split()[2][len("v"):-len(".Hs")]
+        return version
 
 
 if __name__ == "__main__":
