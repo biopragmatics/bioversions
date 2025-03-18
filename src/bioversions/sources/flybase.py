95339856f4923beca60c7cf48181c662c4eae093
Ben Gyori <ben.gyori@gmail.com>
2025-03-18 10:45:10 -0700

    Update FlyBase getter for new deployment

--- src/bioversions/sources/flybase.py
+++ src/bioversions/sources/flybase.py
@@ -1,6 +1,9 @@
 """A getter for FlyBase."""
 
-import ftplib
+import re
+
+from bs4 import BeautifulSoup
+import requests
 
 from bioversions.utils import Getter, VersionType
 
@@ -14,18 +17,29 @@ class FlybaseGetter(Getter):
 
     bioregistry_id = "flybase"
     name = "FlyBase"
-    homepage_fmt = "http://ftp.flybase.net/releases/FB{version}/"
+    homepage_fmt = (
+        "http://flybase-ftp.s3-website-us-east-1.amazonaws.com/releases/FB{version}/"
+    )
     version_type = VersionType.date
 
     def get(self):
         """Get the latest flybase version number."""
-        with ftplib.FTP("ftp.flybase.net") as ftp:
-            ftp.login()
-            ftp.cwd("releases")
-            for name in sorted(ftp.nlst(), reverse=True):
-                if name.startswith("FB2"):
-                    return name[len("FB") :]
-        raise ValueError
+        res = requests.get(
+            "http://flybase-ftp.s3-website-us-east-1.amazonaws.com/releases/index.html"
+        )
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, "html.parser")
+
+        release_pattern = re.compile(r"FB\d{4}_\d{2}")
+        releases = []
+        # We check links to find ones that look like releases
+        for a_tag in soup.find_all("a", href=True):
+            match = release_pattern.search(a_tag.text)
+            if match:
+                # Strip off the leading FB here
+                releases.append(match.group()[2:])
+        latest_version = sorted(releases, reverse=True)[0]
+        return latest_version
 
 
 if __name__ == "__main__":
