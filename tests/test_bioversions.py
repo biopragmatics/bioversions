1cfaf741433c0fc18b196183a1758aae76063512
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-08-11 12:09:08 +0200

    Fix encoding bug (#90)

--- tests/test_bioversions.py
+++ tests/test_bioversions.py
@@ -1,12 +1,16 @@
 """Tests for bioversions."""
 
 import datetime
+import re
 import unittest
 
 import bioregistry
 
 import bioversions
 from bioversions.sources import BioGRIDGetter, WikiPathwaysGetter, get_getters
+from bioversions.utils import get_obo_version, get_obograph_json_version, get_owl_xml_version
+
+YYYYMMDD = re.compile("\\d{4}-\\d{2}-\\d{2}")
 
 
 class TestGetter(unittest.TestCase):
@@ -46,3 +50,31 @@ class TestGetter(unittest.TestCase):
 
                 d = getter.version_date_parsed
                 self.assertIsInstance(d, datetime.date)
+
+    def test_get_obo_version(self) -> None:
+        """Test getting an OBO version."""
+        version = get_obo_version("https://current.geneontology.org/ontology/go.obo")
+        if version is None:
+            raise ValueError
+        version = version.removeprefix("releases/")
+        self.assertRegex(version, YYYYMMDD)
+
+    def test_get_obograph_version(self) -> None:
+        """Test getting an OBO graph version."""
+        version = get_obograph_json_version("http://purl.obolibrary.org/obo/go.json")
+        self.assertRegex(_clean_owl(version), YYYYMMDD)
+
+    def test_get_owl_xml_version(self) -> None:
+        """Test getting an OWL XML version."""
+        version = get_owl_xml_version("https://current.geneontology.org/ontology/go.owl")
+        self.assertRegex(_clean_owl(version), YYYYMMDD)
+
+
+def _clean_owl(version: str | None) -> str:
+    if version is None:
+        raise ValueError
+    return (
+        version.strip('"')
+        .removeprefix("http://purl.obolibrary.org/obo/go/releases/")
+        .removesuffix("/go.owl")
+    )
