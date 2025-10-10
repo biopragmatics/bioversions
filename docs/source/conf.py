433c148eafb0edda4ce2ca705dfae711819b0b8c
GitHub Action <action@github.com>
2025-10-10 01:06:19 +0000

    Bump version: 0.8.159-dev → 0.8.159

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = f"{date.today().year}, Charles Tapley Hoyt"
 author = "Charles Tapley Hoyt"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.8.159-dev"
+release = "0.8.159"
 
 # The short X.Y version.
 parsed_version = re.match(
