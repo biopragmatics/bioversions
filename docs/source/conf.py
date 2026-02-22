1fea839fe5bfdc897d977ac4b83acce742c50e4a
GitHub Action <action@github.com>
2026-02-22 01:46:03 +0000

    Bump version: 0.8.273 → 0.8.274-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = f"{date.today().year}, Charles Tapley Hoyt"
 author = "Charles Tapley Hoyt"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.8.273"
+release = "0.8.274-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
--- src/bioversions/version.py
+++ src/bioversions/version.py
@@ -11,7 +11,7 @@ __all__ = [
     "get_version",
 ]
 
-VERSION = "0.8.273"
+VERSION = "0.8.274-dev"
 
 
 def get_git_hash() -> str | None:
