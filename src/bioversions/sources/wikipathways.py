97b1bb49e6924bf0b9edecd210fa54d4a2750e2e
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-04-22 12:41:07 +0200

    Update wikipathways.py

--- src/bioversions/sources/wikipathways.py
+++ src/bioversions/sources/wikipathways.py
@@ -1,12 +1,26 @@
 """A getter for WikiPathways."""
 
-from bioversions.utils import Getter, VersionType, get_soup
+from bioversions.utils import Getter, VersionType
 
 __all__ = [
     "WikiPathwaysGetter",
+    "get_wikidata_version",
 ]
 
 URL = "http://data.wikipathways.org/current/gmt/"
+
+
+def get_wikidata_version() -> str:
+    """Get the latest WikiPathways version number."""
+    from pystow.utils import get_soup
+
+    soup = get_soup(URL)
+    if soup is None:
+        raise ValueError(f"could not get WikiPathways data from {URL}")
+    anchor = soup.find(id="File")
+    if anchor is None or not isinstance(anchor.text, str):
+        raise ValueError(f"could could not parse WikiPathways data from {URL}")
+    return anchor.text.split("-")[1]
 
 
 class WikiPathwaysGetter(Getter):
@@ -20,11 +34,7 @@ class WikiPathwaysGetter(Getter):
 
     def get(self) -> str:
         """Get the latest WikiPathways version number."""
-        soup = get_soup(URL)
-        if soup is None:
-            raise ValueError(f"could not get WikiPathways data from {URL}")
-        anchor = soup.find(id="File")
-        return anchor.text.split("-")[1]
+        return get_wikidata_version()
 
 
 if __name__ == "__main__":
