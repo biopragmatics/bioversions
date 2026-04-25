2e1593d7293302e7c51bae7a8878fa14a98b2449
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-04-25 21:03:08 +0200

    Update depmap.py

--- src/bioversions/sources/depmap.py
+++ src/bioversions/sources/depmap.py
@@ -5,6 +5,7 @@ from typing import cast
 import requests
 
 from bioversions.utils import Getter, VersionType
+from bioversions.version import get_version
 
 __all__ = [
     "DepMapGetter",
@@ -18,12 +19,19 @@ class DepMapGetter(Getter):
 
     name = "DepMap"
     version_type = VersionType.other
+    bioregistry_id = "depmap"
 
     def get(self) -> str:
         """Get the latest DepMap version number."""
-        res = requests.get(URL, timeout=15)
-        latest = next(release for release in res.json()["releaseData"] if release["isLatest"])
-        return cast(str, latest["releaseName"][len("DepMap Public ") :])
+        res = requests.get(
+            URL,
+            timeout=15,
+            headers={"User-Agent": f"bioversions v{get_version(with_git_hash=True)}"},
+        )
+        res_json = res.json()
+        latest = next(release for release in res_json["releaseData"] if release["isLatest"])
+        rv = cast(str, latest["releaseName"][len("DepMap Public ") :])
+        return rv
 
 
 if __name__ == "__main__":
